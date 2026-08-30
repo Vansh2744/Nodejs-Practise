8d45213a72e70f52a5ab6f05ff518f01b906312d
Vansh <vanshchoudhary2744@gmail.com>
2026-08-31 01:01:45 +0530

    created routing in http server

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import { createServer } from "node:http";
 
 const server = createServer((req, res) => {
-
-  res.writeHead(200, { "content-type": "application/json" });
-  res.end(JSON.stringify({ name: "Vansh", email: "vansh@gmail.com" }));
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "Hello Vansh" }));
+  } else if (req.url === "/user") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ name: "Vansh", email: "vansh@gmail.com" }));
+  } else if (req.url === "/product") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ name: "Headphones", price: 23000 }));
+  } else {
+    res.writeHead(400, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "No url found" }));
+  }
 });
 
 server.listen(3000, () => console.log("listening to port:3000"));
