5900ff17773ef648a073e2dc30d98956fef7b179
Vansh <vanshchoudhary2744@gmail.com>
2026-08-30 11:46:10 +0530

    creating folder using fs

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import fs from "node:fs";
 
-const content = fs.readFileSync("test.txt", "utf-8");
-console.log(content);
+// const content = fs.readFileSync("test.txt", "utf-8");
+// console.log(content);
 
 // fs.writeFileSync("copy.txt", content, "utf-8");
 
-fs.appendFileSync("copy.txt", `\n${content}`, "utf-8");
+// fs.appendFileSync("copy.txt", `\n${content}`, "utf-8");
+
+fs.mkdirSync("app");
+
+fs.mkdirSync("src/utils", { recursive: true });
