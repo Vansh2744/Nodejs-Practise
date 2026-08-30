219cb7a73176acb11dc1d988d160cbb328de14ce
Vansh <vanshchoudhary2744@gmail.com>
2026-08-30 11:43:15 +0530

    append file sync

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ import fs from "node:fs";
 const content = fs.readFileSync("test.txt", "utf-8");
 console.log(content);
 
-fs.writeFileSync("copy.txt", content, "utf-8");
+// fs.writeFileSync("copy.txt", content, "utf-8");
+
+fs.appendFileSync("copy.txt", `\n${content}`, "utf-8");
