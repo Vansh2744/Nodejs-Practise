1db9ec4d713ae1785cd2c50c9db8c0b0f31b7f32
Vansh <vanshchoudhary2744@gmail.com>
2026-08-30 12:07:21 +0530

    some os uses

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import fs from "node:fs";
+import os from "node:os";
 
 // const content = fs.readFileSync("test.txt", "utf-8");
 // console.log(content);
@@ -12,4 +13,11 @@ import fs from "node:fs";
 // fs.mkdirSync("src/utils", { recursive: true });
 
 // fs.rmdirSync("app");
-fs.rmSync("src", { recursive: true, force: true });
+// fs.rmSync("src", { recursive: true, force: true });
+
+// console.log(os.cpus());
+console.log("Total Memory", os.totalmem() / (1024 * 1024 * 1024)); //GB
+console.log("Free Memory", os.freemem() / (1024 * 1024)); //MB
+console.log(os.uptime() / (60 * 60)); //Hours
+console.log(os.hostname());
+console.log(os.userInfo());
