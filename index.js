df7e3f9c5c83fd00fa04bd5188213fa562995462
Vansh <vanshchoudhary2744@gmail.com>
2026-08-30 18:37:16 +0530

    reading file without blocking

--- index.js
+++ index.js
@@ -16,8 +16,23 @@ import os from "node:os";
 // fs.rmSync("src", { recursive: true, force: true });
 
 // console.log(os.cpus());
-console.log("Total Memory", os.totalmem() / (1024 * 1024 * 1024)); //GB
-console.log("Free Memory", os.freemem() / (1024 * 1024)); //MB
-console.log(os.uptime() / (60 * 60)); //Hours
-console.log(os.hostname());
-console.log(os.userInfo());
+// console.log("Total Memory", os.totalmem() / (1024 * 1024 * 1024)); //GB
+// console.log("Free Memory", os.freemem() / (1024 * 1024)); //MB
+// console.log(os.uptime() / (60 * 60)); //Hours
+// console.log(os.hostname());
+// console.log(os.userInfo());
+
+// console.log(os.cpus()[1]);
+
+// console.log("Hello Vansh");
+
+// console.log(os.cpus()[1]);
+
+fs.readFile("test.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("Something went wrong");
+    return;
+  } else {
+    console.log(data);
+  }
+});
