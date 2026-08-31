80667056482bfa157b53c019893c788b5c0d5f55
Vansh <vanshchoudhary2744@gmail.com>
2026-08-31 12:55:28 +0530

    stream using event listener

--- stream.js
+++ stream.js
@@ -1,6 +1,19 @@
 import fs from "fs";
 
-const readStream = fs.createReadStream("input.json");
+const readStream = fs.createReadStream("input.json", {
+  encoding: "utf-8",
+  highWaterMark: 20,
+});
 const writeStream = fs.createWriteStream("output.json");
 
-readStream.pipe(writeStream);
+// readStream.pipe(writeStream);
+
+readStream.on("data", (chunk) => {
+  console.log(Buffer.from(chunk));
+  writeStream.write(chunk);
+});
+
+readStream.on("end", () => {
+  console.log("Written in output.json file");
+  writeStream.end();
+});
