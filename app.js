e78855b016271e582c6ad7db724ed11faab6b5f1
Vansh <vanshchoudhary2744@gmail.com>
2026-09-01 08:58:53 +0530

    error handling using middleware

--- app.js
+++ app.js
@@ -9,17 +9,32 @@ const ageCheck = (req, res, next) => {
   if (age < 18) {
     return res.json({ message: "Invalid Age" });
   }
+  req.otp = 4567;
   next();
 };
 
-app.post("/", ageCheck, (req, res) => {
+const checkUser = (req, res, next) => {
+  if (req.otp !== 4547) {
+    return res.json({ message: "Invalid User" });
+  }
+  next();
+};
+
+app.post("/", (req, res) => {
   const { email, username, age } = req.body;
 
-  return res.json({
-    message: "You are registered successfully",
-    email,
-    username,
-  });
+  // return res.json({
+  //   message: "You are registered successfully",
+  //   email,
+  //   username,
+  // });
+
+  throw new Error("Unable to create user");
+});
+
+app.use((err, req, res, nest) => {
+  console.error(err.message);
+  res.status(500).json({ success: false, message: err.message });
 });
 
 app.listen(3000, () => {
