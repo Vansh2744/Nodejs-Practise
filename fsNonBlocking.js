70d0eced7871afdaa5742458a3034ec78f3e9a6e
Vansh <vanshchoudhary2744@gmail.com>
2026-09-05 19:00:24 +0530

    written data in file using non-blocking code

--- fsNonBlocking.js
+++ fsNonBlocking.js
@@ -1,11 +1,20 @@
 import fs from "fs";
 
-fs.readFile("test.txt", "utf-8", (err, data) => {
+// fs.readFile("test.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     throw new Error(err.message);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// console.log("Ending File Read");
+
+fs.writeFile("main.txt", "Hello Vansh", (err) => {
   if (err) {
     throw new Error(err.message);
-  } else {
-    console.log(data);
   }
+  console.log("Written in File");
 });
 
-console.log("Ending File Read");
+console.log({ name: "Vansh", email: "vansh@gmail.com" });
