92325e175fc811f8921ed0f26822416b99ad37af
Vansh <vanshchoudhary2744@gmail.com>
2026-09-06 01:44:52 +0530

    multi called event

--- event.js
+++ event.js
@@ -8,10 +8,20 @@ const eventEmitter = new EventEmitter();
 
 // eventEmitter.emit("msg", { name: "Vansh", email: "vansh@gmail,com" });
 
-eventEmitter.once("msg", (msg) => {
-  console.log(msg);
+// eventEmitter.once("msg", (msg) => {
+//   console.log(msg);
+// });
+
+// eventEmitter.emit("msg", "Hello Vansh");
+// eventEmitter.emit("msg", "Hello Aman");
+// eventEmitter.emit("msg", "Hello Kartik");
+
+eventEmitter.on("msg", (msg) => {
+  console.log(`Message : ${msg}`);
+});
+
+eventEmitter.on("msg", (msg) => {
+  console.log(`Received : ${msg}`);
 });
 
 eventEmitter.emit("msg", "Hello Vansh");
-eventEmitter.emit("msg", "Hello Aman");
-eventEmitter.emit("msg", "Hello Kartik");
