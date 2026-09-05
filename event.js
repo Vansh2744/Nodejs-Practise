70aefc93890efe11e03753e37aa6e132132a48d7
Vansh <vanshchoudhary2744@gmail.com>
2026-09-06 01:54:08 +0530

    Error handling using event

--- event.js
+++ event.js
@@ -1,6 +1,6 @@
 import EventEmitter from "node:events";
 
-// const eventEmitter = new EventEmitter();
+const eventEmitter = new EventEmitter();
 
 // eventEmitter.on("msg", (user) => {
 //   console.log(user);
@@ -26,17 +26,32 @@ import EventEmitter from "node:events";
 
 // eventEmitter.emit("msg", "Hello Vansh");
 
-class Chat extends EventEmitter {
-  sendMessage(msg) {
-    console.log("Message Send : ", msg);
-    this.emit("msg", msg);
-  }
-}
+// class Chat extends EventEmitter {
+//   sendMessage(msg) {
+//     console.log("Message Send : ", msg);
+//     this.emit("msg", msg);
+//   }
 
-const chat = new Chat();
+//   sendNotification(msg) {
+//     this.emit("notify", msg);
+//   }
+// }
 
-chat.on("msg", (msg) => {
-  console.log(`Message Received : ${msg}`);
+// const chat = new Chat();
+
+// chat.on("msg", (msg) => {
+//   console.log(`Message Received : ${msg}`);
+// });
+
+// chat.on("notify", (msg) => {
+//   console.log(`Notified that ${msg}`);
+// });
+
+// chat.sendMessage("Hello Vansh");
+// chat.sendNotification("You are logged in successfully");
+
+eventEmitter.on("error", (err) => {
+  console.log(`Error : ${err.message}`);
 });
 
-chat.sendMessage("Hello Vansh");
+eventEmitter.emit("error", new Error("Unable to Fetch User"));
